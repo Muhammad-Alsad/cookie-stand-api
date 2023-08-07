f9846fcf326d76b227f0a4acae98d5daaec488f7
muhammad <moalsad001@gmail.com>
2023-08-07 22:58:30 +0000

    all done

--- CookieStands/views_front.py
+++ CookieStands/views_front.py
@@ -24,7 +24,7 @@ class CookieStandUpdateView(LoginRequiredMixin, UpdateView):
 class CookieStandCreateView(LoginRequiredMixin, CreateView):
     template_name = "CookieStands/CookieStand_create.html"
     model = CookieStand
-    fields = ["name", "rating", "reviewer"] # "__all__" for all of them
+    fields = "__all__" # "__all__" for all of them
 
 
 class CookieStandDeleteView(LoginRequiredMixin, DeleteView):
